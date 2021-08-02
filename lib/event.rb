80244331f5298ca62d6af8d5017883c9e799c67e
Weston Ellis <ellisweston112@gmail.com>
2021-08-02 14:44:06 -0600

    Finish iteration 3

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,49 @@ class Event
     end
   end
 
+  def total_inventory
+    total_inventory = {}
+
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, amount|
+        total_inventory[item] = {
+        quantity: 0,
+        food_trucks: []
+        }
+      end
+    end
+
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, amount|
+        total_inventory.each do |item_, inventory|
+          if item_ == item
+            inventory[:quantity] += amount
+          end
+        end
+      end
+    end
+
+    total_inventory.each do |item, inventory|
+      inventory[:food_trucks] = food_trucks_that_sell(item)
+    end
+
+
+    total_inventory
+  end
+
+  def overstocked_items
+    overstock =[]
+    total_inventory.each do |item, inventory|
+      if inventory[:quantity] > 50 && inventory[:food_trucks].length > 1
+        overstock << item
+      end
+    end
+    overstock
+  end
+
+  def sorted_item_list
+    total_inventory.keys.map do |item, inventory|
+      item.name
+    end.sort
+  end
 end
